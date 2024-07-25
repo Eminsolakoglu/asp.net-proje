dfd057105a2ceb6e41b75d055bc207724ef4f146
Emin Solakoğlu <132267025+Eminsolakoglu@users.noreply.github.com>
2024-07-25 14:04:16 +0300

    test commit

--- CoreDemo/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CoreDemo.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
