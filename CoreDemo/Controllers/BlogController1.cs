a0aac8c6c251b18b2ef930bd7b3527df648fa8e9
EminSolakoglu <muhammedeminsolakoglu82@gmail.com>
2024-07-25 13:38:22 +0300

    Temanın projeye eklenmesi ve partiallar bölünmesi

---
+++ CoreDemo/Controllers/BlogController1.cs
@@ -0,0 +1,14 @@
+﻿using DataAccessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+    public class BlogController1 : Controller
+    {
+        IBlogDal _blogdal;
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
