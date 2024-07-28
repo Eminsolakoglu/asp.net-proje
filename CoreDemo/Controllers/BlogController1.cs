d9e621985fc2a0d854bd5d2b46c1a5a6e560b7e8
EminSolakoglu <muhammedeminsolakoglu82@gmail.com>
2024-07-28 22:36:49 +0300

    blog detayları

--- CoreDemo/Controllers/BlogController1.cs
+++ CoreDemo/Controllers/BlogController1.cs
@@ -13,5 +13,10 @@ namespace CoreDemo.Controllers
             var values = bm.GetBlogListWithCategory();
             return View(values);
         }
+        public IActionResult BlogDetails(int id)
+        {
+            return View();
+        }
+
     }
 }
