9c6097edec4f4fea4e515ac73ba193e4829300cb
EminSolakoglu <muhammedeminsolakoglu82@gmail.com>
2024-07-30 16:05:18 +0300

    yazar kayıt sayfası düzenleme

---
+++ CoreDemo/Controllers/RegisterController.cs
@@ -0,0 +1,27 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CoreDemo.Controllers
+{
+    public class RegisterController : Controller
+    {
+        WriterManager wm = new WriterManager(new EfWriterRepository());
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+		public IActionResult Index(Writer p)
+		{
+            p.WriterStatus = true;
+            p.WriterAbout = "Deneme Teset";
+            wm.WriterAdd(p);
+			return RedirectToAction("Index","Blog");
+		}
+		
+	}
+}
