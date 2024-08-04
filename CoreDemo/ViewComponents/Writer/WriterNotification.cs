d4f0aa0ffa2fb83bffbb9f15feb2d0f734fbd2ff
EminSolakoglu <muhammedeminsolakoglu82@gmail.com>
2024-08-04 18:10:31 +0300

    Yazar Üst Menü düzenlenmesi

---
+++ CoreDemo/ViewComponents/Writer/WriterNotification.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents.Writer
+{
+    public class WriterNotification: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+
+            return View();
+        }
+    }
+}
